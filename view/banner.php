d3ed9a587f90cb3996dafe587141013966bdcc17
Dangtien <dvtmlbb@gmail.com>
2023-12-05 04:12:50 +0700

    feat: responsive part 1

--- view/banner.php
+++ view/banner.php
@@ -1,19 +1,20 @@
 <div class="swiper mySwiper banner">
         <div class="swiper-wrapper banner-wrapper">
+            <div class="swiper-slide">
+                <img src="../img/banner/Property 1=Default.png" alt="">
+            </div>
             <div class="swiper-slide banner">
                 <img src="../img/banner/baner2.png" alt="">
             </div>
             <div class="swiper-slide">
-                <img src="../img/banner/baner2.png" alt="">
+                <img src="../img/banner/baner3.png" alt="">
             </div>
-            <div class="swiper-slide">
-                <img src="../img/banner/baner2.png" alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="../img/banner/baner2.png" alt="">
-            </div>
+            
+            <!-- <div class="swiper-slide">
+                <img src="./img/banner/baner2.png" alt="">
+            </div> -->
         </div>
-        <div class="swiper-button-next"></div>
+        <!-- <div class="swiper-button-next"></div>
         <div class="swiper-button-prev"></div>
-        <div class="swiper-pagination"></div>
+        <div class="swiper-pagination"></div> -->
     </div>
